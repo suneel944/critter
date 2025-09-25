b6d553210d9da40d69564f08650e6a544e842217
suneel944 <suneel944@gmail.com>
2025-09-25 21:36:03 +0400

    test(ui): update LoginPage page object
    Signed-off-by: suneel944 <suneel944@gmail.com>

--- tests/ui/pages/automation-exercise/LoginPage.ts
+++ tests/ui/pages/automation-exercise/LoginPage.ts
@@ -1,5 +1,6 @@
-import { Page } from "@playwright/test";
-import { BasePage } from "../BasePage";
+import { BasePage } from '../BasePage'
+import type { Page } from '@playwright/test'
+
 
 /**
  * LoginPage models the login form on Automation Exercise.
@@ -10,17 +11,17 @@ import { BasePage } from "../BasePage";
  */
 export class LoginPage extends BasePage {
   constructor(page: Page) {
-    super(page);
+    super(page)
   }
 
   // Selectors
-  private readonly loginLink = "a[href='/login']";
-  private readonly editField = (fieldName: "email" | "password"): string =>
-    `input[data-qa="login-${fieldName}"]`;
-  private readonly loginSubmitButton = "button[data-qa='login-button']";
+  private readonly loginLink = "a[href='/login']"
+  private readonly editField = (fieldName: 'email' | 'password'): string =>
+    `input[data-qa="login-${fieldName}"]`
+  private readonly loginSubmitButton = "button[data-qa='login-button']"
 
   async goto(): Promise<void> {
-    await this.page.click(this.loginLink);
+    await this.page.click(this.loginLink)
   }
 
   /**
@@ -32,8 +33,8 @@ export class LoginPage extends BasePage {
    * @param password Registered user’s password
    */
   async login(email: string, password: string): Promise<void> {
-    await this.page.fill(this.editField("email"), email);
-    await this.page.fill(this.editField("password"), password);
-    await this.page.click(this.loginSubmitButton);
+    await this.page.fill(this.editField('email'), email)
+    await this.page.fill(this.editField('password'), password)
+    await this.page.click(this.loginSubmitButton)
   }
 }
